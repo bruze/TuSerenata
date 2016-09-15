56e69c138e4fec73b4ade41d2d7c24fd488dbf60
Bruce <bgarelli@altimetrik.com>
2016-09-15 17:15:02 -0300

    Primero

---
+++ TuSerenata/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuSerenata
+//
+//  Created by Bruno Garelli on 9/15/16.
+//  Copyright © 2016 Bruno Garelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
